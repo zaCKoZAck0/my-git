e72e3527e096819fef7975f98fa4e9132ce93a8e
zackozack <id.ayushkryadav@gmail.com>
2024-09-12 08:19:10 +0530

    .git

--- app/main.ts
+++ app/main.ts
@@ -13,11 +13,11 @@ switch (command) {
         console.log("Logs from your program will appear here!");
 
         // Uncomment this block to pass the first stage
-        // fs.mkdirSync(".git", { recursive: true });
-        // fs.mkdirSync(".git/objects", { recursive: true });
-        // fs.mkdirSync(".git/refs", { recursive: true });
-        // fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
-        // console.log("Initialized git directory");
+        fs.mkdirSync(".git", { recursive: true });
+        fs.mkdirSync(".git/objects", { recursive: true });
+        fs.mkdirSync(".git/refs", { recursive: true });
+        fs.writeFileSync(".git/HEAD", "ref: refs/heads/main\n");
+        console.log("Initialized git directory");
         break;
     default:
         throw new Error(`Unknown command ${command}`);
