280c01aed9a690c59356ad19ef8376b6bd01c9c8
zackozack <id.ayushkryadav@gmail.com>
2024-09-14 06:15:25 +0530

    hash-object

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,7 @@ const command = args[0];
 enum Commands {
     INIT = "init",
     CAT_FILE = "cat-file",
+    HASH_OBJECT = "hash-object",
 }
 
 const repo = new GitRepository();
@@ -19,7 +20,12 @@ switch (command) {
         const hash = args[2];
         repo.catFile(hash);
         break;
-        
+    
+    case Commands.HASH_OBJECT:
+        const write = args.includes("--write") || args.includes("-w");
+        const filePath = args[args.length - 1];
+        repo.hashObject(filePath, write);
+        break;
     default:
         throw new Error(`Unknown command ${command}`);
 }
--- app/repo.ts
+++ app/repo.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { unzipSync } from 'zlib';
+import { createHash } from 'crypto';
 
 export class GitRepository {
     init() {
@@ -15,5 +16,19 @@ export class GitRepository {
         const decompressed = unzipSync(object);
         const nullByteIndex = decompressed.indexOf(0);
         process.stdout.write(decompressed.subarray(nullByteIndex + 1).toString()); 
+    }
+
+    hashObject(filePath: string, write: boolean) {
+        const data = fs.readFileSync(filePath);
+        const header = `blob ${data.length}\0`;
+        const store = header + data;
+        const hash = createHash('sha1').update(store).digest('hex');
+        const objectPath = `.git/objects/${hash.slice(0, 2)}/${hash.slice(2)}`;
+        // Check if the same object already exists in repository and if the write flag is set
+        if (write && !fs.existsSync(objectPath)) {
+            fs.mkdirSync(`.git/objects/${hash.slice(0, 2)}`, { recursive: true });
+            fs.writeFileSync(objectPath, store);
+        }
+        process.stdout.write(hash);
     }
 }
