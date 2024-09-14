515846f8a9085ffcb004dabc5d613446443778ae
zackozack <id.ayushkryadav@gmail.com>
2024-09-14 06:17:27 +0530

    hash-object

--- app/repo.ts
+++ app/repo.ts
@@ -25,7 +25,7 @@ export class GitRepository {
         const hash = createHash('sha1').update(store).digest('hex');
         const objectPath = `.git/objects/${hash.slice(0, 2)}/${hash.slice(2)}`;
         // Check if the same object already exists in repository and if the write flag is set
-        if (write && !fs.existsSync(objectPath)) {
+        if (!fs.existsSync(objectPath)) {
             fs.mkdirSync(`.git/objects/${hash.slice(0, 2)}`, { recursive: true });
             fs.writeFileSync(objectPath, store);
         }
